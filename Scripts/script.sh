07d857ac53d63396638b2702a7eeb611000f3509
iam-syed <simplysahil313@gmail.com>
2026-06-22 12:02:16 +0530

    Update script.sh

--- Scripts/script.sh
+++ Scripts/script.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/user/bin/env bash
 
+################################
+# Author: Awez Syed
+# contact: syedawez9022@gmail.com
+# version: v1
+#
+# About: Its a very useful scripts whenever you have to create  a Dir and mv files inside it.
+#################################
 mkdir Scripts
 sleep 1s
 echo "Dir is ready"
