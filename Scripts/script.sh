b3f241232fa897a477f6ef1628607f74bae87679
iam-syed <simplysahil313@gmail.com>
2026-06-22 12:05:35 +0530

    Update script.sh

--- Scripts/script.sh
+++ Scripts/script.sh
@@ -5,16 +5,16 @@
 # contact: syedawez9022@gmail.com
 # version: v1
 #
-# About: Its a very useful scripts whenever you have to create  a Dir and mv files inside it.
+# About: Its a very useful Script, whenever you have to create a Dir and move files inside it.
 #################################
 mkdir Scripts
 sleep 1s
 echo "Dir is ready"
 
-for i in Simple_and_Random-script
+for i in YOUR_FOLDER_NAME
 do
-	mv * Scripts
+	mv * Scripts 
 	sleep 1s
-	#echo "moved files in a Dir"
+	echo "moved ALL files inside Dir"
 done
 
