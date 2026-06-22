c07878e691b6e668aa4eae9bad9228eb8cda073f
Sahil_313 <simplysahil313.gmail.com>
2026-06-22 10:49:43 +0530

    Fix ShellCheck SC2068 warning

--- Scripts/Basic-Scripts/args.sh
+++ Scripts/Basic-Scripts/args.sh
@@ -25,8 +25,10 @@ echo "No. of arguments: $#"
 # runing forloop to access the values from the arguments
 # USECASE: when you have a script for backing up the files with these this usecase, while executing the script provide the path of that file
 # to perform one-by-one operations on that file.
+#Example:  ./args "My file.txt" test.txt
 
-for filename in $@
+
+for filename in "$@"
 do
 	echo "copying the file: $filename"
 
